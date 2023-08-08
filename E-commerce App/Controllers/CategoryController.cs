2c2c77f00101429478f1cb2128093d1e8157e86c
vinh <vinhht14@gmail.com>
2023-08-08 11:37:59 +0700

    code

---
+++ E-commerce App/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using E_commerce_App.Data;
+using E_commerce_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce_App.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objectCategoryList = _db.Categories.ToList();
+            return View(objectCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
