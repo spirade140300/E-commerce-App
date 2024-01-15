1c7b4cb04f93d18461fa8209958023c052d03a09
htvinh <vinhht14@gmail.com>
2024-01-15 10:50:36 +0700

    Allow admin to create new user

--- E-commerce App/Areas/Customer/Controllers/CartController.cs
+++ E-commerce App/Areas/Customer/Controllers/CartController.cs
@@ -214,6 +214,7 @@ namespace E_commerce_App.Areas.Customer.Controllers
                     _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
                     _unitOfWork.Save();
                 }
+                HttpContext.Session.Clear();
             }
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
                 .GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
--- E-commerce App/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ E-commerce App/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -205,8 +205,15 @@ namespace E_commerce_App.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New user created successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                            return LocalRedirect(returnUrl);
+                        }
                     }
                 }
                 foreach (var error in result.Errors)
